a4e5f7c6746de7f11f447dfb4b90a275874e834c
okshi <johnstarhafiz@gmail.com>
2021-10-16 10:37:56 +0600

    This is firebase env. setup commit

--- src/Pages/Home/Firebase/firebase.config.js
+++ src/Pages/Home/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-     apiKey: "AIzaSyA5kx-8Felvxd_IeJd-TvwC5UtBkVQqmxY",
-     authDomain: "red-onion-authentication.firebaseapp.com",
-     projectId: "red-onion-authentication",
-     storageBucket: "red-onion-authentication.appspot.com",
-     messagingSenderId: "599552050940",
-     appId: "1:599552050940:web:1f9b00a1c5c45565db3979"
+apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.REACT_APP_FIREBASE_APP_ID
    };
 
 export default firebaseConfig;
